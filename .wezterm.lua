9f730564487d8d8f6c2927d915262493320e228c
mozaldy <moxaldy@gmail.com>
2024-05-19 13:23:07 +0700

    first commit

---
+++ .wezterm.lua
@@ -0,0 +1,71 @@
+local wezterm = require("wezterm")
+
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = wezterm.mux.spawn_window(cmd or {})
+	window:gui_window():toggle_fullscreen()
+end)
+
+return {
+	adjust_window_size_when_changing_font_size = false,
+	audible_bell = "Disabled",
+	background = {
+		{
+			source = { File = "/home/mozaldy/Downloads/logh.jpg" },
+			horizontal_align = "Center",
+			vertical_align = "Middle",
+			height = "Contain",
+			width = "Contain",
+			repeat_x = "NoRepeat",
+			repeat_y = "NoRepeat",
+			opacity = 1,
+		},
+		{
+			source = {
+				Color = "Black",
+			},
+			height = "100%",
+			width = "100%",
+			opacity = 0.8,
+		},
+		-- {
+		-- 	source = { File = "/home/mozaldy/Downloads/3840x2160.png" },
+		-- 	horizontal_align = "Center",
+		-- 	vertical_align = "Middle",
+		-- 	height = "Contain",
+		-- 	width = "Contain",
+		-- 	repeat_x = "NoRepeat",
+		-- 	repeat_y = "NoRepeat",
+		-- 	opacity = 0.9,
+		-- },
+	},
+	--   op
+	-- color_scheme = "Tokyo Night",
+	-- color_scheme = "Alabaster",
+	exit_behavior = "Close",
+	font_size = 12,
+	force_reverse_video_cursor = true,
+	hide_mouse_cursor_when_typing = true,
+	hide_tab_bar_if_only_one_tab = true,
+	keys = {
+		{ action = wezterm.action.ActivateCommandPalette, mods = "CTRL|SHIFT", key = "P" },
+		{ action = wezterm.action.CopyTo("Clipboard"), mods = "CTRL|SHIFT", key = "C" },
+		{ action = wezterm.action.DecreaseFontSize, mods = "CTRL", key = "-" },
+		{ action = wezterm.action.IncreaseFontSize, mods = "CTRL", key = "=" },
+		{ action = wezterm.action.Nop, mods = "ALT", key = "Enter" },
+		{ action = wezterm.action.PasteFrom("Clipboard"), mods = "CTRL|SHIFT", key = "V" },
+		{ action = wezterm.action.ResetFontSize, mods = "CTRL", key = "0" },
+		{ action = wezterm.action.ToggleFullScreen, key = "F11" },
+	},
+	scrollback_lines = 10000,
+	show_update_window = true,
+	use_dead_keys = false,
+	unicode_version = 15,
+	macos_window_background_blur = 100,
+	window_close_confirmation = "NeverPrompt",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = "0.6cell",
+		bottom = 0,
+	},
+}
