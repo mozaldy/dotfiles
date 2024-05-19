6cea3948f2e6aaa2923539ef4f27904d74d678f6
Mohammad Rizaldy Ramadhan <moxaldy@gmail.com>
2024-05-19 20:18:01 +0700

    Delete Code - OSS directory

--- Code - OSS/User/History/-67efb1ca/S9Ke.sql
+++
@@ -1,3 +0,0 @@
-SELECT ContactName, Address, City
-FROM Customers
-WHERE Country NOT IN ('Germany', 'Mexico', 'Spain');
--- Code - OSS/User/History/-67fdc94b/HhiV.sql
+++
@@ -1,4 +0,0 @@
-SELECT OrderID, OrderDate, ShippedDate, CustomerID, Freight
-FROM Orders
-ORDER BY Freight DESC
-LIMIT 10;
